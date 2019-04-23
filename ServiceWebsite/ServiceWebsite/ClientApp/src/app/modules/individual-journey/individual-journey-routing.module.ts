d287064aa8b921bbb02dc621b97d2ab591eef3f3
David Dikman <ddikman@gmail.com>
2019-04-23 19:05:12 +0100

    Added in pipe and components to routing

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/individual-journey-routing.module.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/individual-journey-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// components
+import { AboutHearingsComponent } from './components/about-hearings/about-hearings.component';
+
+// directives/pipes
+import { LocalisePipe } from './pipes/localise.pipe';
+
 const routes: Routes = [
+  { path: 'about-hearings', component: AboutHearingsComponent }
 ];
 
 @NgModule({
+  declarations: [
+    LocalisePipe,
+    AboutHearingsComponent
+  ],
   exports: [
     RouterModule
   ],
--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/individual-journey.module.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/individual-journey.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+// app modules
 import { SharedModule } from '../shared/shared.module';
 import { IndividualJourneyRoutingModule } from './individual-journey-routing.module';
 
