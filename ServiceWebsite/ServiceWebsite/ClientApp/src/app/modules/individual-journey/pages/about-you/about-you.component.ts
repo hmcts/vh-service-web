6877866d35a43c0774b93b68c798d035a3dcfca2
David Dikman <ddikman@gmail.com>
2019-04-25 18:25:01 +0100

    Reverted about you example

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/about-you/about-you.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/about-you/about-you.component.ts
@@ -1,47 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { IndividualBaseComponent } from '../individual-base-component/individual-base.component';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-about-you',
   templateUrl: './about-you.component.html',
   styles: []
 })
-export class AboutYouComponent extends IndividualBaseComponent implements OnDestroy, OnInit {
-  abilityForm: FormGroup;
-  ability: FormControl;
-  moreDetails: FormControl;
-  abilityInputChanges: Subscription;
-  moreDetailsChanges: Subscription;
-
-  ngOnInit() {
-    //
-    // The following is an example usage of the model.
-    // Any values updated/stored will be handled on .continue() or .fail() by the journey object
-    //
-    this.ability = new FormControl(this.model.aboutYou.answer, [ Validators.required ]);
-    this.moreDetails = new FormControl(this.model.aboutYou.notes, [ Validators.required ]);
-
-    this.abilityForm = new FormGroup({
-      ability: this.ability,
-      moreDetails: this.moreDetails
-    });
-
-    this.abilityInputChanges = this.ability.valueChanges.subscribe(hasAbilityIssues => {
-      this.model.aboutYou.answer = hasAbilityIssues;
-      if (!hasAbilityIssues) {
-        this.model.aboutYou.notes = null;
-      }
-    });
-
-    this.moreDetailsChanges = this.moreDetails.valueChanges.subscribe(notes => {
-      this.model.aboutYou.notes = notes;
-    });
-  }
-
-  ngOnDestroy() {
-    this.abilityInputChanges.unsubscribe();
-    this.moreDetailsChanges.unsubscribe();
-  }
+export class AboutYouComponent extends IndividualBaseComponent {
 }
