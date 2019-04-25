9809b3be3ecfd5174ee10f513ec9de0c6e996cda
David Dikman <ddikman@gmail.com>
2019-04-25 16:33:58 +0100

    Another test for logging without properties

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/services/console-logger.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/services/console-logger.spec.ts
@@ -3,6 +3,10 @@ import { ConsoleLogger } from './console-logger';
 describe('ConsoleLogger', () => {
   const logger: ConsoleLogger = new ConsoleLogger();
 
+  it('can log events', () => {
+    logger.trackEvent('testEvent');
+  });
+
   it('can log events with properties', () => {
     logger.trackEvent('testEvent', { property: 'value' });
   });
