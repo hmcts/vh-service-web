2d9f025bf91b26155d24e57ada1b033d7d8b016d
David Dikman <ddikman@gmail.com>
2019-04-26 17:11:17 +0100

    Updated test name

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/participant-view/participant-view.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/participant-view/participant-view.component.spec.ts
@@ -1,10 +1,10 @@
+import { MutableIndividualSuitabilityModel } from './../../mutable-individual-suitability.model';
 import { CanCreateComponent } from '../individual-base-component/component-test-bed.spec';
 import { ParticipantViewComponent } from './participant-view.component';
-import { TestModuleMetadata, TestBed } from '@angular/core/testing';
+import { TestModuleMetadata } from '@angular/core/testing';
 import { MediaService } from '../../services/media.service';
 import { Component, Input } from '@angular/core';
 import { IndividualJourney } from '../../individual-journey';
-import { IndividualSuitabilityModel} from '../../individual-suitability.model';
 
 @Component({
   selector: 'app-user-camera-view',
@@ -31,9 +31,6 @@ class StubAudioBarComponent {
 class StubContactUsComponent {
 }
 
-class StepMock extends IndividualSuitabilityModel {
-}
-
 describe('ParticipantViewComponent', () => {
   it('can be created', () => {
     CanCreateComponent(ParticipantViewComponent, (configuration: TestModuleMetadata) => {
@@ -49,19 +46,20 @@ describe('ParticipantViewComponent', () => {
 
   describe('functionality', () => {
     let component: ParticipantViewComponent;
-    const journey = new IndividualJourney(new StepMock());
     const userMediaService = jasmine.createSpyObj<MediaService>(['getStream', 'stopStream']);
-
     const mediaStream = new MediaStream();
 
     beforeEach(() => {
+      const journey = new IndividualJourney(new MutableIndividualSuitabilityModel());
       component = new ParticipantViewComponent(journey, userMediaService);
     });
-    it('should get source for video', () => {
+
+    it('should set the video source to a media stream when initialized', () => {
       userMediaService.getStream.and.returnValue(Promise.resolve(mediaStream));
       component.ngAfterContentInit();
       expect(userMediaService.getStream).toHaveBeenCalled();
     });
+
     it('should stop use camera on destroy', () => {
       userMediaService.getStream.and.returnValue(Promise.resolve(mediaStream));
       component.ngAfterContentInit();
