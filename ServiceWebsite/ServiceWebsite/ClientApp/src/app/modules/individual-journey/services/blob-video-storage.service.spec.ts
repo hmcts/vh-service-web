89884967a850c27db22f2cad8bad22792360967c
Lionel Roche <lionel.roche@hmcts.net>
2019-04-26 14:55:55 +0100

    rebuild

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/blob-video-storage.service.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/blob-video-storage.service.spec.ts
@@ -7,7 +7,6 @@ describe('BlobVideoStorageService', () => {
     it('should return the videoname from constants', () => {
         const config = new Config();
         config.baseVideoUrl = 'http://test.com';
-
         const blobVideoStorageService = new BlobVideoStorageService(new BlobStorageService(config));
         const actualValue = blobVideoStorageService.getVideoUrl('BeforeTheDay_ParticipantView');
         expect(actualValue).toEqual('http://test.com/btd_individual_laptop_large.mp4');
