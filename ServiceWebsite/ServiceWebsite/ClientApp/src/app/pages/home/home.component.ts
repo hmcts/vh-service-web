3c2fed2025df927674451cf183f5e9a5126f3495
David Dikman <ddikman@gmail.com>
2019-04-29 18:35:43 +0100

    Journey now loaded based on current user profile

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/home/home.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ProfileService } from 'src/app/services/profile.service';
 import { Component, OnInit } from '@angular/core';
 import { JourneyFactory } from 'src/app/modules/base-journey/services/journey.factory';
 
@@ -6,12 +7,15 @@ import { JourneyFactory } from 'src/app/modules/base-journey/services/journey.fa
   template: ''
 })
 export class HomeComponent implements OnInit {
-  constructor(private journeyFactory: JourneyFactory) {}
+  constructor(private journeyFactory: JourneyFactory, private profileService: ProfileService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    const profile = await this.profileService.getUserProfile();
+    const journey = this.journeyFactory.getJourney(profile.role);
+    journey.begin();
     // mock to individual user
-    const currentUserType = 'Individual';
-    this.journeyFactory.getJourney(currentUserType).begin();
+    // const currentUserType = 'Individual';
+    // this.journeyFactory.getJourney(currentUserType).begin();
   }
 }
 
