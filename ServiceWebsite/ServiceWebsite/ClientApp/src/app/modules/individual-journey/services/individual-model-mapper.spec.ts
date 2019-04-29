5be8c48d73045efd90d3850b9b1446e8f53201f9
David Dikman <ddikman@gmail.com>
2019-04-29 20:38:26 +0100

    Added all mappings

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/individual-model-mapper.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/individual-model-mapper.spec.ts
@@ -1,3 +1,4 @@
+import { HasAccessToComputer } from './../individual-suitability.model';
 import { HearingSuitabilityResponse, HearingSuitabilityAnswer } from './../../../services/clients/api-client';
 import { IndividualModelMapper, IndividualQuestionKeys as Keys } from './individual-model-mapper';
 import { IndividualSuitabilityModel } from '../individual-suitability.model';
@@ -49,9 +50,30 @@ describe('IndividualModelMapper', () => {
         model = new IndividualModelMapper().map(serviceResponse);
     };
 
-    const givenAnswerIs = (answerKey: string, answer: string) => {
-        serviceResponse.answers.find(a => a.question_key === answerKey).answer = answer;
+    const givenAnswerIs = (answerKey: string, answer: string, extended?: string) => {
+        serviceResponse.answers.find(a => a.question_key === answerKey).answer = answer;;
     };
+
+    const givenExtendedAnswerIs = (answerKey: string, extendedAnswer: string) => {
+        serviceResponse.answers.find(a => a.question_key === answerKey).extended_answer = extendedAnswer;
+    };
+
+    it('should map computer answers', () => {
+        const values = [ 'Yes', 'No', 'Not sure' ];
+        const expected = [ HasAccessToComputer.Yes, HasAccessToComputer.No, HasAccessToComputer.NotSure ];
+
+        for (let i = 0; i < expected.length; ++i) {
+            givenAnswerIs(Keys.Computer, values[i]);
+            whenMappingModel();
+            expect(model.computer).toBe(expected[i]);
+        }
+    });
+
+    it('should map interpreted', () => {
+        givenAnswerIs(Keys.Interpreter, 'false');
+        whenMappingModel();
+        expect(model.interpreter).toBeFalsy();
+    });
 
     it('should map false answers', () => {
         givenAnswerIs(Keys.AboutYou, 'false');
@@ -70,11 +92,16 @@ describe('IndividualModelMapper', () => {
 
         whenMappingModel();
 
-        // then
         expect(model.aboutYou.answer).toBe(true);
         expect(model.consent.answer).toBe(true);
         expect(model.internet.answer).toBe(true);
         expect(model.room.answer).toBe(true);
         expect(model.interpreter).toBe(true);
+    });
+
+    it('should map extended answer', () => {
+        givenExtendedAnswerIs(Keys.AboutYou, 'more information');
+        whenMappingModel();
+        expect(model.aboutYou.notes).toBe('more information');
     });
 });
