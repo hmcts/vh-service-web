c8482df26dde7def265dccc6f824ddea2f839f9e
kkodalikk <karuna.kodali@hmcts.net>
2019-04-30 12:49:07 +0100

    Fixed format issue

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/user-media.service.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/user-media.service.spec.ts
@@ -4,19 +4,19 @@ import { LoggerService } from 'src/app/services/logger.service';
 
 describe('UserMediaService', () => {
     it('should return the media stream', async () => {
-    const logger = jasmine.createSpyObj<LoggerService>(['error']);
-    const userMediaService = new UserMediaService(logger);
-    const stream = await userMediaService.getStream();
-     expect(stream).not.toBeNull();
-     userMediaService.stopStream();
+        const logger = jasmine.createSpyObj<LoggerService>(['error']);
+        const userMediaService = new UserMediaService(logger);
+        const stream = await userMediaService.getStream();
+        expect(stream).not.toBeNull();
+        userMediaService.stopStream();
     });
 
     it('should return media access request successfull', async () => {
         const logger = jasmine.createSpyObj<LoggerService>(['error']);
         const userMediaService = new UserMediaService(logger);
         const result = await userMediaService.requestAccess();
-         expect(result).not.toBeNull();
-         expect(result).toBeTruthy();
-         userMediaService.stopStream();
-        });
+        expect(result).not.toBeNull();
+        expect(result).toBeTruthy();
+        userMediaService.stopStream();
+    });
 });
