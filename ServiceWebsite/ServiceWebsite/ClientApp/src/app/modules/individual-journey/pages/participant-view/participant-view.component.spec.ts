41c4b3616def45564c011f60b0e7d1e8d4f2bbfe
kkodalikk <karuna.kodali@hmcts.net>
2019-04-30 10:11:11 +0100

    Partial Fix for unit test

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/participant-view/participant-view.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/participant-view/participant-view.component.spec.ts
@@ -3,7 +3,7 @@ import { CanCreateComponent } from '../individual-base-component/component-test-
 import { ParticipantViewComponent } from './participant-view.component';
 import { TestModuleMetadata } from '@angular/core/testing';
 import { MediaService } from '../../services/media.service';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { IndividualJourney } from '../../individual-journey';
 import { AudioBarComponent } from '../../components/audio-bar/audio-bar.component';
 import { UserCameraViewComponent } from '../../components/user-camera-view/user-camera-view.component';
@@ -16,6 +16,15 @@ import {ContactUsComponent} from '../../../shared/contact-us/contact-us.componen
 class StubUserCameraViewComponent {
   @Input()
   videoWidth: string;
+
+  @ViewChild('videoBox')
+  videoBox: ElementRef;
+
+  stream: MediaStream;
+  setSource(stream: MediaStream) 
+  {
+
+  }
 }
 
 @Component({
@@ -25,6 +34,18 @@ class StubUserCameraViewComponent {
 class StubAudioBarComponent {
   @Input()
   audioBarWidth: string;
+
+  @ViewChild('visualizer')
+  visualizer: ElementRef;
+
+  audioContext: AudioContext;
+  widthCanvas: number;
+  heightCanvas: number;
+  canvasContext: CanvasRenderingContext2D;
+  volume = 1;
+  colorAudio = '#006435';
+  setSource(stream: MediaStream) {
+  }
 }
 
 @Component({
@@ -32,6 +53,7 @@ class StubAudioBarComponent {
   template: ''
 })
 class StubContactUsComponent {
+ 
 }
 
 describe('ParticipantViewComponent', () => {
@@ -51,20 +73,25 @@ describe('ParticipantViewComponent', () => {
     let component: ParticipantViewComponent;
     const userMediaService = jasmine.createSpyObj<MediaService>(['getStream', 'stopStream', 'requestAccess']);
     const mediaStream = new MediaStream();
+    const audioBarComponentSpy = jasmine.createSpyObj<AudioBarComponent>(['setSource']);
+    const userCameraViewComponentSpy = jasmine.createSpyObj<UserCameraViewComponent>(['setSource']);
     beforeEach(() => {
       const journey = new IndividualJourney(new MutableIndividualSuitabilityModel());
       component = new ParticipantViewComponent(journey, userMediaService);
+      component.userCameraViewComponent = userCameraViewComponentSpy;
+      component.audioBarComponent = audioBarComponentSpy;
+      
     });
 
-    it('should set the video source to a media stream when initialized', () => {
+    it('should set the video source to a media stream when initialized', async() => {
       userMediaService.getStream.and.returnValue(Promise.resolve(mediaStream));
-      component.ngAfterContentInit();
+      await component.ngAfterContentInit();
       expect(userMediaService.getStream).toHaveBeenCalled();
     });
 
-    it('should stop use camera on destroy', () => {
+    it('should stop use camera on destroy', async() => {
       userMediaService.getStream.and.returnValue(Promise.resolve(mediaStream));
-      component.ngAfterContentInit();
+      await component.ngAfterContentInit();
 
       component.ngOnDestroy();
       expect(userMediaService.stopStream).toHaveBeenCalled();
