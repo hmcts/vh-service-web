6f8f63d925cbeabe7a69056b161a2968b84d1955
kkodalikk <karuna.kodali@hmcts.net>
2019-04-30 11:42:55 +0100

    Fixed lint issue

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/user-media.service.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/user-media.service.spec.ts
@@ -5,10 +5,10 @@ import { LoggerService } from 'src/app/services/logger.service';
 describe('UserMediaService', () => {
     it('should return the media stream', async () => {
     const logger = jasmine.createSpyObj<LoggerService>(['error']);
-    const userMediaService = new UserMediaService(logger)
+    const userMediaService = new UserMediaService(logger);
     const stream = await userMediaService.getStream();
-     expect(stream).not.toBeNull()
+     expect(stream).not.toBeNull();
      userMediaService.stopStream();
     });
-    
+
 });
