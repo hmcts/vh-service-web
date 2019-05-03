c27eadfe07cd3ecfbcca60f6584e0b2e6f84abf3
David Dikman <ddikman@gmail.com>
2019-05-03 14:41:29 +0100

    Corrected spacing

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/shared/crest-blue-panel/crest-blue-panel.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/shared/crest-blue-panel/crest-blue-panel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-crest-blue-panel',
   templateUrl: './crest-blue-panel.component.html',
-  styleUrls: ['./crest-blue-panel.component.css']
+  styleUrls: ['./crest-blue-panel.component.scss']
 })
 export class CrestBluePanelComponent {
 }
