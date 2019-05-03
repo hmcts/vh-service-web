b6a3f4f4b4a526531cce1aa26b00222bae2a04d0
Lionel Roche <lionel.roche@hmcts.net>
2019-05-03 15:14:48 +0100

    updated test to mock the logger instead of the logger service

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/video-view/video-view.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/video-view/video-view.component.spec.ts
@@ -1,9 +1,10 @@
 import { VideoViewComponent } from './video-view.component';
 import { LoggerService } from 'src/app/services/logger.service';
+import { Logger } from 'src/app/services/logger';
 
 describe('VideoViewComponent', () => {
   it('should emit loaded event', () => {
-    const logger = jasmine.createSpyObj<LoggerService>(['error']);
+    const logger = jasmine.createSpyObj<Logger>(['error']);
     const videoViewComponent = new VideoViewComponent(logger);
     spyOn(videoViewComponent.loaded, 'emit');
     videoViewComponent.readyToPlay();
@@ -11,7 +12,7 @@ describe('VideoViewComponent', () => {
   });
 
   it('should set video unvailable on error', () => {
-    const logger = jasmine.createSpyObj<LoggerService>(['error']);
+    const logger = jasmine.createSpyObj<Logger>(['error']);
     const videoViewComponent = new VideoViewComponent(logger);
     videoViewComponent.onVideoError();
     expect(videoViewComponent.videoUnavailable).toBeTruthy();
