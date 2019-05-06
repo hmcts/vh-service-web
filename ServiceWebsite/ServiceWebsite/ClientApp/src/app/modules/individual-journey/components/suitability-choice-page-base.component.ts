fff2c0c565e1b9c9f3af48c597e0eee13d03cd0a
David Dikman <ddikman@gmail.com>
2019-05-06 12:54:48 +0100

    Example of simple two choice form

---
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/suitability-choice-page-base.component.ts
@@ -0,0 +1,32 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IndividualBaseComponent } from '../pages/individual-base-component/individual-base.component';
+
+export abstract class SuitabilityChoicePageBaseComponent extends IndividualBaseComponent {
+  readonly choice = new FormControl('', Validators.required);
+
+  submitted = false;
+
+  readonly form = new FormGroup({
+    choice: this.choice
+  });
+
+  get isFormInvalid(): boolean {
+    return this.form.invalid && this.submitted;
+  }
+
+  continue() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.bindModel();
+    super.continue();
+  }
+
+  /**
+   * Bind the value to the model
+   */
+  protected abstract bindModel();
+}
