a13d733541d0e5960f0b20e59defd8d579e5e5e3
abitec <abiodun.ve@gmail.com>
2019-05-09 08:59:50 +0100

    Fix failed test

--- ServiceWebsite/ServiceWebsite.AcceptanceTests/Pages/IndividualPages/ParticipantView.cs
+++ ServiceWebsite/ServiceWebsite.AcceptanceTests/Pages/IndividualPages/ParticipantView.cs
@@ -16,7 +16,6 @@ namespace ServiceWebsite.AcceptanceTests.Pages.IndividualPages
             _commonPages = commonPages;
         }
         private const string PageUrl = "/participant-view";
-        private const string ParticpantViewDetails = "When the video has ended, show the judge's view to see how you will appear to others in the hearing.";
         private By _videoContent => By.Id("video-element");
         public void IndividualViewsInformationVideo()
         {
@@ -25,14 +24,17 @@ namespace ServiceWebsite.AcceptanceTests.Pages.IndividualPages
 
         public void VideoHasStarted()
         {
+            _context.Retry(() =>
+            {
                 double currentTime;
-            if (!double.TryParse(GetMethods.GetAttributeValue(_videoContent, _context, "currentTime"),
-                    out currentTime))
+                if (!double.TryParse(GetMethods.GetAttributeValue(_videoContent, _context, "currentTime"),
+                        out currentTime))
                     throw new Exception();
 
                 if (currentTime > 0)
-                    return; 
-                throw new Exception($"Video current time is ${currentTime}, it hasn't started automatically");
+                    return;
+                throw new Exception($"Video current time is {currentTime}, it hasn't started automatically");
+            });
         }
 
         //Judge View page will be removed soon, this method allows navigating to Judge View page
