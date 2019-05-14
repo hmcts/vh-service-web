41e50d99a82b6e30eac13c57787cacbeb38d4d34
Valentina <Valentina.bennett@hmcts.net>
2019-05-14 10:50:22 +0100

    unit test for deviceType

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/device-type.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/device-type.spec.ts
@@ -1,42 +1,20 @@
+import { TestBed, inject } from '@angular/core/testing';
 import { DeviceType } from './device-type';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
 describe('DeviceType', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DeviceDetectorService]
+    });
+  });
 
-  it('should return true for a mobile device', () => {
-    const deviceDetectorService = jasmine.createSpyObj<DeviceDetectorService>(['isMobile', 'isTablet', 'isDesktop']);
+  it('should return false for a mobile device', inject([DeviceDetectorService], (deviceDetectorService: DeviceDetectorService) => {
     const deviceType = new DeviceType(deviceDetectorService);
-    deviceDetectorService.isMobile.and.returnValue(true);
-    expect(deviceType.isMobile()).toBeTruthy();
-  });
-  it('should return true for a tablet device', () => {
-    const deviceDetectorService = jasmine.createSpyObj<DeviceDetectorService>(['isMobile', 'isTablet', 'isDesktop']);
-    const deviceType = new DeviceType(deviceDetectorService);
-    deviceDetectorService.isTablet.and.returnValue(true);
-    expect(deviceType.isTablet()).toBeTruthy();
-  });
-  it('should return true for a desktop device', () => {
-    const deviceDetectorService = jasmine.createSpyObj<DeviceDetectorService>(['isMobile', 'isTablet', 'isDesktop']);
-    const deviceType = new DeviceType(deviceDetectorService);
-    deviceDetectorService.isDesktop.and.returnValue(true);
-    expect(deviceType.isDesktop()).toBeTruthy();
-  });
-  it('should return false for a mobile device', () => {
-    const deviceDetectorService = jasmine.createSpyObj<DeviceDetectorService>(['isMobile', 'isTablet', 'isDesktop']);
-    const deviceType = new DeviceType(deviceDetectorService);
-    deviceDetectorService.isMobile.and.returnValue(false);
     expect(deviceType.isMobile()).toBeFalsy();
-  });
-  it('should return false for a tablet device', () => {
-    const deviceDetectorService = jasmine.createSpyObj<DeviceDetectorService>(['isMobile', 'isTablet', 'isDesktop']);
+  }));
+  it('should return true for a desktop device', inject([DeviceDetectorService], (deviceDetectorService: DeviceDetectorService) => {
     const deviceType = new DeviceType(deviceDetectorService);
-    deviceDetectorService.isTablet.and.returnValue(false);
-    expect(deviceType.isTablet()).toBeFalsy();
-  });
-  it('should return false for a desktop device', () => {
-    const deviceDetectorService = jasmine.createSpyObj<DeviceDetectorService>(['isMobile', 'isTablet', 'isDesktop']);
-    const deviceType = new DeviceType(deviceDetectorService);
-    deviceDetectorService.isDesktop.and.returnValue(false);
-    expect(deviceType.isDesktop()).toBeFalsy();
-  });
+    expect(deviceType.isDesktop()).toBeTruthy();
+  }));
 });
