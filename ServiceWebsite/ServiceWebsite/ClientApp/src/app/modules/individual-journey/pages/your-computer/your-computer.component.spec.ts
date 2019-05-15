d98da964591ff3309da40041712b075eba141735
Lionel Roche <lionel.roche@hmcts.net>
2019-05-15 14:52:42 +0100

    Initial Version.

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/your-computer/your-computer.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/your-computer/your-computer.component.spec.ts
@@ -1,8 +1,11 @@
 import { CanCreateComponent } from '../individual-base-component/component-test-bed.spec';
 import { YourComputerComponent } from './your-computer.component';
+import { CannotProceeedUntilChoiceIsSelected } from '../../components/suitability-choice-page-base.component.spec';
 
 describe('YourComputerComponent', () => {
-  it('can be created', () => {
-    CanCreateComponent(YourComputerComponent);
+  it('cannot proceed to next step until pressing choice, after submit value is bound', () => {
+    const fixture = CannotProceeedUntilChoiceIsSelected(YourComputerComponent);
+    // and value is bound
+    expect(fixture.component.model.computer).toBe(true);
   });
 });
--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/your-computer/your-computer.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/your-computer/your-computer.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { IndividualBaseComponent } from '../individual-base-component/individual-base.component';
+import { SuitabilityChoicePageBaseComponent } from '../../components/suitability-choice-page-base.component';
 
 @Component({
   selector: 'app-your-computer',
   templateUrl: './your-computer.component.html',
   styles: []
 })
-export class YourComputerComponent extends IndividualBaseComponent {
-
-  yes() {
-    this.model.computer = true;
-    this.continue();
-  }
-  no() {
-    this.model.computer = false;
-    this.continue();
+export class YourComputerComponent extends SuitabilityChoicePageBaseComponent {
+  protected bindModel() {
+    this.model.computer = this.choice.value;
   }
 }
