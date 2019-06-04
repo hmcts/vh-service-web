f08cf8387516e9e84b97a0e14a00505c836c2106
Valentina <Valentina.bennett@hmcts.net>
2019-06-04 11:58:06 +0100

    access to a room

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/access-to-room/access-to-room.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/access-to-room/access-to-room.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-access-to-room',
@@ -7,4 +8,32 @@ import { RepresentativeBaseComponent } from '../representative-base-component/re
   styles: []
 })
 export class AccessToRoomComponent extends RepresentativeBaseComponent {
+
+  readonly choice = new FormControl('', Validators.required);
+
+  submitted = false;
+
+  readonly form = new FormGroup({
+    choice: this.choice
+  });
+
+  get isFormInvalid(): boolean {
+    return this.form.invalid && this.submitted;
+  }
+
+  continue() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+   // this.bindModel();
+    super.continue();
+  }
+
+  /**
+   * Bind the value to the model
+   */
+ // protected abstract bindModel(): void;
 }
