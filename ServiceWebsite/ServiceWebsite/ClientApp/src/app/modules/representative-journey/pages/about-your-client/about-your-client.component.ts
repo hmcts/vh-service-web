c5577edf3f403092a937dbe097d63921af87c464
Lionel Roche <lionel.roche@hmcts.net>
2019-06-04 23:15:22 +0100

    Initial Version: Added html.

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/about-your-client/about-your-client.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/about-your-client/about-your-client.component.ts
@@ -1,10 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
+import { SuitabilityChoicePageBaseComponent } from 'src/app/modules/individual-journey/components/suitability-choice-page-base.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-about-your-client',
   templateUrl: './about-your-client.component.html',
   styles: []
 })
-export class AboutYourClientComponent extends RepresentativeBaseComponent {
+export class AboutYourClientComponent extends SuitabilityChoicePageBaseComponent implements OnInit {
+
+  readonly textInput = new FormControl('');
+
+  ngOnInit(): void {
+
+  }
+  protected bindModel(): void {
+
+  }
 }
