d0e3525585e81eb3a88b85bb08e37af0bc6bc0f5
Lionel Roche <lionel.roche@hmcts.net>
2019-06-05 14:44:35 +0100

    Added about your client screen.

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/about-your-client/about-your-client.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/about-your-client/about-your-client.component.spec.ts
@@ -1,8 +1,83 @@
 import { AboutYourClientComponent } from './about-your-client.component';
-import { CanCreateComponent } from '../representative-base-component/component-test-bed.spec';
+import {
+  ConfigureTestBedForPageComponent,
+  SuitabilityChoicePageBaseFixture
+} from '../../components/suitability-choice-page-base.component.spec';
 
 describe('AboutYourClientComponent', () => {
-  it('can be created', () => {
-    CanCreateComponent(AboutYourClientComponent);
+  let fixture: SuitabilityChoicePageBaseFixture<AboutYourClientComponent>;
+  let component: AboutYourClientComponent;
+
+  beforeEach(() => {
+    fixture = ConfigureTestBedForPageComponent(AboutYourClientComponent);
+    component = fixture.component;
+    fixture.detectChanges();
+  });
+
+  it('cannot proceed to next step before selecting a choice', () => {
+    // when
+    fixture.submitIsClicked();
+
+    // then
+    expect(component.isFormInvalid).toBeTruthy();
+
+    // expect form to display error class
+    const formContainer = fixture.debugElementByCss('#form-container');
+    expect(formContainer.classes['govuk-form-group--error']).toBeTruthy();
+
+    // and error message to be displayed
+    const errorMessage = fixture.debugElementByCss('#form-container .govuk-error-message');
+    expect(errorMessage.nativeElement).toBeTruthy();
+  });
+
+  it('should clear error when selecting no', () => {
+    fixture.submitIsClicked();
+    fixture.radioBoxIsClicked('#choice-no');
+    expect(component.isFormInvalid).toBeFalsy();
+  });
+
+  it('should clear error when selecting yes after having submitted', () => {
+    fixture.submitIsClicked();
+    fixture.radioBoxIsClicked('#choice-yes');
+    expect(component.isFormInvalid).toBeFalsy();
+  });
+
+  it('should display text field when selecting yes', () => {
+    fixture.radioBoxIsClicked('#choice-yes');
+    expect(component.isFormInvalid).toBeFalsy();
+  });
+
+  it('should be invalid if submitting without having entered any text', () => {
+    // when
+    fixture.radioBoxIsClicked('#choice-yes');
+    fixture.submitIsClicked();
+
+    // then
+    expect(component.isFormInvalid).toBeTruthy();
+    const textfield = fixture.debugElementByCss('#details');
+    expect(textfield.classes['govuk-textarea--error']).toBeTruthy();
+  });
+
+  it('should bind value and notes to model on submit', () => {
+    // when
+    fixture.radioBoxIsClicked('#choice-yes');
+    component.textInput.setValue('notes');
+    fixture.detectChanges();
+    fixture.submitIsClicked();
+
+    // then
+    expect(component.model.aboutYourClient.answer).toBe(true);
+    expect(component.model.aboutYourClient.notes).toBe('notes');
+  });
+
+  it('should bind notes as null on selecting no', () => {
+    // when
+    fixture.radioBoxIsClicked('#choice-no');
+    fixture.detectChanges();
+    fixture.submitIsClicked();
+
+    // then
+    expect(component.model.aboutYourClient.answer).toBe(false);
+    expect(component.model.aboutYourClient.notes).toBeNull();
   });
 });
--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/about-your-client/about-your-client.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/about-your-client/about-your-client.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
-import { SuitabilityChoicePageBaseComponent } from 'src/app/modules/individual-journey/components/suitability-choice-page-base.component';
-import { FormControl } from '@angular/forms';
+import {
+  SuitabilityChoicePageBaseComponent
+} from 'src/app/modules/representative-journey/components/suitability-choice-page-base.component';
+import { FormControl, Validators } from '@angular/forms';
+import { ValidateForWhiteSpace } from 'src/app/modules/shared/validators/whitespace-validator';
 
 @Component({
   selector: 'app-about-your-client',
@@ -12,10 +14,44 @@ export class AboutYourClientComponent extends SuitabilityChoicePageBaseComponent
 
   readonly textInput = new FormControl('');
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form.addControl('textInput', this.textInput);
 
+    this.choice.setValue(this.model.aboutYourClient.answer);
+    this.textInput.setValue(this.model.aboutYourClient.notes);
+
+    this.choice.valueChanges.subscribe(value => {
+      if (value) {
+        // If the value is true, the text input is required
+        this.textInput.setValidators([Validators.required, ValidateForWhiteSpace]);
+        this.textInput.markAsUntouched();
+      } else {
+        this.textInput.clearValidators();
+      }
+
+      // calling these will update the forms validity based on the changed validators
+      this.textInput.updateValueAndValidity();
+      this.form.updateValueAndValidity();
+
+      // if we've tried submitting without and answer select an answer that has another required field
+      // we want to treat this as "a new form", the user hasn't seen these fields so we need to treat the
+      // form as pristine until the next time the user has tried to submit, this reduces flickering of the
+      // error messages as the user focuses/blurs and enters text
+      this.submitted = false;
+    });
   }
-  protected bindModel(): void {
 
+  get isTextInputInvalid(): boolean {
+    return this.textInput.invalid && this.submitted;
+  }
+
+  continue() {
+    this.textInput.markAsTouched();
+    super.continue();
+  }
+
+  protected bindModel(): void {
+    this.model.aboutYourClient.answer = this.choice.value;
+    this.model.aboutYourClient.notes = this.choice.value ? this.textInput.value : null;
   }
 }
