4c82432988ff7eaa3f3873569b2b5786e12e0a65
Valentina <Valentina.bennett@hmcts.net>
2019-06-05 16:09:51 +0100

    amended Thank you page

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/thank-you/thank-you.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/thank-you/thank-you.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
 
 @Component({
   selector: 'app-thank-you',
   templateUrl: './thank-you.component.html',
-  styles: []
+  styles: [],
 })
-export class ThankYouComponent extends RepresentativeBaseComponent implements OnInit {
-  hearingDate: Date;
-
-  ngOnInit() {
-    // this.hearingDate = this.model.hearing.scheduleDateTime;
-  }
+export class ThankYouComponent extends RepresentativeBaseComponent {
 
   protected bindModel(): void { }
 }
