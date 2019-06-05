13b9b2b2deb182b5dd70ff126a4eed0cd5bf6b93
Valentina <Valentina.bennett@hmcts.net>
2019-06-05 11:23:31 +0100

    update base class- for access room

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/access-to-room/access-to-room.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/access-to-room/access-to-room.component.ts
@@ -1,39 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { SuitabilityChoicePageBaseComponent } from '../../components/suitability-choice-page-base.component';
 
 @Component({
   selector: 'app-access-to-room',
   templateUrl: './access-to-room.component.html',
   styleUrls: ['./access-to-room.component.css']
 })
-export class AccessToRoomComponent extends RepresentativeBaseComponent {
+export class AccessToRoomComponent extends SuitabilityChoicePageBaseComponent {
 
-  readonly choice = new FormControl('', Validators.required);
-
-  submitted = false;
-
-  readonly form = new FormGroup({
-    choice: this.choice
-  });
-
-  get isFormInvalid(): boolean {
-    return this.form.invalid && this.submitted;
+  protected bindModel(): void {
+    this.model.room = this.choice.value;
   }
-
-  continue() {
-    this.submitted = true;
-
-    if (this.form.invalid) {
-      return;
-    }
-
-   // this.bindModel();
-    super.continue();
-  }
-
-  /**
-   * Bind the value to the model
-   */
- // protected abstract bindModel(): void;
 }
