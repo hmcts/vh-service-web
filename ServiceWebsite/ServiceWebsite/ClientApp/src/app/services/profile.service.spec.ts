4694d074f0e2724e51dfb2a2e20fd629c421a196
David Dikman <ddikman@gmail.com>
2019-06-06 09:27:44 +0100

    Added in some missing tests

---
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/services/profile.service.spec.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { ProfileService } from './profile.service';
+import { of } from 'rxjs';
+
+describe('ProfileService', () => {
+  let client: jasmine.SpyObj<HttpClient>;
+  let service: ProfileService;
+
+  const serviceResponse = {
+    email: 'email',
+    role: 'role'
+  };
+
+  beforeAll(() => {
+    client = jasmine.createSpyObj<HttpClient>(['get']);
+    client.get.and.returnValue(of(serviceResponse));
+
+    service = new ProfileService(client);
+  });
+
+  it('user is not logged in until profile is loaded', async () => {
+    expect(service.isLoggedIn).toBe(false);
+    await service.getUserProfile();
+    expect(service.isLoggedIn).toBe(true);
+  });
+
+  it('maps role and email to profile', async () => {
+    const profile = await service.getUserProfile();
+    expect(profile.email).toBe(serviceResponse.email);
+    expect(profile.role).toBe(serviceResponse.role);
+  });
+});
