fb63652b45e3ad1067f47652ccd2c67b47906003
Lionel Roche <lionel.roche@hmcts.net>
2019-06-06 09:18:29 +0100

    Added 'Your Computer' page for Rep journey

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/your-computer/your-computer.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/your-computer/your-computer.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
+import {
+  SuitabilityChoicePageBaseComponent
+} from 'src/app/modules/representative-journey/components/suitability-choice-page-base.component';
 
 @Component({
   selector: 'app-your-computer',
   templateUrl: './your-computer.component.html',
   styles: []
 })
-export class YourComputerComponent extends RepresentativeBaseComponent {
-  yes() {
-    this.model.computer = true;
-    console.log(this.model.computer);
-    this.continue();
+export class YourComputerComponent extends SuitabilityChoicePageBaseComponent implements OnInit {
+
+  ngOnInit() {
+    this.choice.setValue(this.model.computer);
   }
-  no() {
-    this.model.computer = false;
-    console.log(this.model.computer);
-    this.continue();
+
+  protected bindModel(): void {
+    this.model.computer = this.choice.value;
   }
 }
