642b5f775b073213e3a35822738c1b439f67391e
Lionel Roche <lionel.roche@hmcts.net>
2019-06-11 12:05:43 +0100

    initial version

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/hearing-suitability/hearing-suitability.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/hearing-suitability/hearing-suitability.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { RepresentativeBaseComponent } from '../representative-base-component/representative-base.component';
+import {
+  SuitabilityChoiceTextboxPageBaseComponent
+} from 'src/app/modules/base-journey/components/suitability-choice-textbox-page-base.component';
+import { RepresentativeJourney } from '../../representative-journey';
 
 @Component({
   selector: 'app-hearing-suitability',
   templateUrl: './hearing-suitability.component.html',
   styles: []
 })
-export class HearingSuitabilityComponent extends RepresentativeBaseComponent {
+export class HearingSuitabilityComponent
+  extends SuitabilityChoiceTextboxPageBaseComponent<RepresentativeJourney> implements OnInit {
+
+  constructor(journey: RepresentativeJourney) {
+    super(journey);
+  }
+
+  ngOnInit() {
+    this.form.addControl('textInput', this.textInput);
+
+    super.ngOnInit();
+    this.choice.setValue(this.journey.model.hearingSuitability.answer);
+    this.textInput.setValue(this.journey.model.hearingSuitability.notes);
+  }
+
+  protected bindModel(): void {
+    this.journey.model.hearingSuitability.answer = this.choice.value;
+    this.journey.model.hearingSuitability.notes = this.choice.value ? this.textInput.value : null;
+  }
 }
