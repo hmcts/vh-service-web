903933c7e04074de82fe106c2f55b74db59220d5
atif.rehman <atif.rehman@hmcts.net>
2019-06-13 11:46:19 +0100

    VIH-4443:Increase code coverage

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/questionnaire-completed/questionnaire-completed.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/questionnaire-completed/questionnaire-completed.component.spec.ts
@@ -1,6 +1,7 @@
-import { RepresentativeJourneyComponentTestBed } from './../representative-base-component/component-test-bed.spec';
-import { QuestionnaireCompletedComponent } from './questionnaire-completed.component';
+import {RepresentativeJourneyComponentTestBed} from './../representative-base-component/component-test-bed.spec';
+import {QuestionnaireCompletedComponent} from './questionnaire-completed.component';
 import {AppYesNoPipe} from '../../../shared/boolean.pipe';
+import {HasAccessToCamera} from '../../../base-journey/participant-suitability.model';
 
 describe('QuestionnaireCompletedComponent', () => {
   it('can be created', () => {
@@ -9,5 +10,21 @@ describe('QuestionnaireCompletedComponent', () => {
       declarations: [AppYesNoPipe]
     });
     expect(fixture).toBeTruthy();
+  });
+
+  it('gets the camera answer values', () => {
+    const fixture = RepresentativeJourneyComponentTestBed.createComponent({
+      component: QuestionnaireCompletedComponent,
+      declarations: [AppYesNoPipe]
+    });
+
+    fixture.componentInstance.model.camera = HasAccessToCamera.Yes;
+    expect(fixture.componentInstance.GetCameraAnswer()).toBe('Yes');
+
+    fixture.componentInstance.model.camera = HasAccessToCamera.No;
+    expect(fixture.componentInstance.GetCameraAnswer()).toBe('No');
+
+    fixture.componentInstance.model.camera = HasAccessToCamera.NotSure;
+    expect(fixture.componentInstance.GetCameraAnswer()).toBe('Not Sure');
   });
 });
