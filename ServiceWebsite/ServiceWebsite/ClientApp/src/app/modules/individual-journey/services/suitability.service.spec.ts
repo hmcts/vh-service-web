789a6fa510fa23189fe28715a3e619f9768cd9db
Sunil DSouza <sunil.dsouza@hmcts.net>
2019-06-14 12:31:35 +0100

    Service that calls suitability api to save answers

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/suitability.service.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/suitability.service.spec.ts
@@ -4,21 +4,26 @@ import { ApiClient } from './../../../services/clients/api-client';
 import { of } from 'rxjs';
 
 describe('SuitabilityService', () => {
-    const client = jasmine.createSpyObj<ApiClient>(['getUserSuitabilityAnswers']);
-    const service = new SuitabilityService(client);
+  const client = jasmine.createSpyObj<ApiClient>(['getUserSuitabilityAnswers', 'updateSuitabilityAnswers']);
+  const service = new SuitabilityService(client);
 
-    it('maps responses from api', async () => {
-        // given some response from server
-        client.getUserSuitabilityAnswers.and.returnValue(of([
-            new HearingSuitabilityResponse({
-                hearing_id: '123',
-                hearing_scheduled_at: new Date(),
-                answers: []
-            })
-        ]));
+  it('maps responses from api', async () => {
+    // given some response from server
+    client.getUserSuitabilityAnswers.and.returnValue(of([
+      new HearingSuitabilityResponse({
+        hearing_id: '123',
+        hearing_scheduled_at: new Date(),
+        answers: []
+      })
+    ]));
 
-        const result = await service.getAllSuitabilityAnswers();
-        expect(result.length).toBe(1);
-        expect(result[0].hearing.id).toBe('123');
-    });
+    const result = await service.getAllSuitabilityAnswers();
+    expect(result.length).toBe(1);
+    expect(result[0].hearing.id).toBe('123');
+  });
+
+  it('submits answers to api', async () => {
+    const result = await service.updateSuitabilityAnswers('123', []);
+    expect(client.updateSuitabilityAnswers).toHaveBeenCalled();
+  });
 });
