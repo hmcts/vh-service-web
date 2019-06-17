c32d5dec97797b78bece404a67243bbac3be7de9
Lionel Roche <lionel.roche@hmcts.net>
2019-06-17 13:15:41 +0100

    updated

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/consent/consent.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/consent/consent.component.ts
@@ -23,12 +23,12 @@ export class ConsentComponent extends SuitabilityChoicePageBaseComponent impleme
     this.form.addControl('textInputYes', this.textInputYes);
     this.form.addControl('textInputNo', this.textInputNo);
 
-/*     this.choice.setValue(this.model.consent.answer);
+    this.choice.setValue(this.model.consent.answer);
     if (this.model.consent.answer === true) {
       this.textInputYes.setValue(this.model.consent.notes);
     } else {
       this.textInputNo.setValue(this.model.consent.notes);
-    } */
+    }
 
     this.choice.valueChanges.subscribe(value => {
       if (value) {
