caa7a4aef4bd222f47974bf7738adcdd072c739b
atif.rehman <atif.rehman@hmcts.net>
2019-06-19 14:30:54 +0100

    VIH-4532: Unit tests

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/representative-journey.factory.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/representative-journey.factory.spec.ts
@@ -1,31 +1,35 @@
-import { RepresentativeSuitabilityService } from './services/representative-suitability.service';
-import { RepresentativeJourneyFactory } from './representative-journey.factory';
-import { RepresentativeJourney } from './representative-journey';
-import { JourneyRoutingListenerService } from '../base-journey/services/journey-routing-listener.service';
-import { RepresentativeJourneyStepComponentBindings } from './services/representative-journey-component-bindings';
+import {RepresentativeSuitabilityService} from './services/representative-suitability.service';
+import {RepresentativeJourneyFactory} from './representative-journey.factory';
+import {RepresentativeJourney} from './representative-journey';
+import {JourneyRoutingListenerService} from '../base-journey/services/journey-routing-listener.service';
+import {RepresentativeJourneyStepComponentBindings} from './services/representative-journey-component-bindings';
+import {RepresentativeJourneyService} from './services/representative.journey.service';
 
 describe('RepresentativeJourneyFactory', () => {
-    let suitabilityService: jasmine.SpyObj<RepresentativeSuitabilityService>;
-    let routingListener: jasmine.SpyObj<JourneyRoutingListenerService>;
-    let journey: jasmine.SpyObj<RepresentativeJourney>;
-    let factory: RepresentativeJourneyFactory;
-    const bindings = new RepresentativeJourneyStepComponentBindings();
+  let suitabilityService: jasmine.SpyObj<RepresentativeSuitabilityService>;
+  let routingListener: jasmine.SpyObj<JourneyRoutingListenerService>;
+  let journey: jasmine.SpyObj<RepresentativeJourney>;
+  let factory: RepresentativeJourneyFactory;
+  const bindings = new RepresentativeJourneyStepComponentBindings();
+  let representativeJourneyService: RepresentativeJourneyService;
 
-    beforeEach(() => {
-        suitabilityService = jasmine.createSpyObj<RepresentativeSuitabilityService>(['getAllSuitabilityAnswers']);
-        suitabilityService.getAllSuitabilityAnswers.and.returnValue(Promise.resolve([]));
-        routingListener = jasmine.createSpyObj<JourneyRoutingListenerService>(['initialise']);
-        journey = jasmine.createSpyObj<RepresentativeJourney>(['forSuitabilityAnswers']);
-        factory = new RepresentativeJourneyFactory(journey, suitabilityService, bindings, routingListener);
-    });
+  beforeEach(() => {
+    suitabilityService = jasmine.createSpyObj<RepresentativeSuitabilityService>(['getAllSuitabilityAnswers']);
+    suitabilityService.getAllSuitabilityAnswers.and.returnValue(Promise.resolve([]));
+    routingListener = jasmine.createSpyObj<JourneyRoutingListenerService>(['initialise']);
+    journey = jasmine.createSpyObj<RepresentativeJourney>(['forSuitabilityAnswers']);
+    representativeJourneyService = jasmine.createSpyObj<RepresentativeJourneyService>(['get', 'set']);
 
-    it('initialises routing and journey', async () => {
-        await factory.begin();
-        expect(routingListener.initialise).toHaveBeenCalled();
-        expect(journey.forSuitabilityAnswers).toHaveBeenCalledWith([]);
-    });
+    factory = new RepresentativeJourneyFactory(journey, suitabilityService, bindings, routingListener, representativeJourneyService);
+  });
 
-    it('handles representative users', () => {
-        expect(factory.handles('Representative')).toBeTruthy();
-    });
+  it('initialises routing and journey', async () => {
+    await factory.begin();
+    expect(routingListener.initialise).toHaveBeenCalled();
+    expect(journey.forSuitabilityAnswers).toHaveBeenCalledWith([]);
+  });
+
+  it('handles representative users', () => {
+    expect(factory.handles('Representative')).toBeTruthy();
+  });
 });
