b2b1efee8fcb3320ec8dbc1672d47cc37c93933a
Atif Rehman <atif.rehman@hmcts.net>
2019-06-21 12:17:42 +0100

    VIH-4532: Unit tests

---
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/services/individual-journey.service.spec.ts
@@ -0,0 +1,37 @@
+import { IndividualJourneyService } from './individual-journey.service';
+import { MutableIndividualSuitabilityModel } from '../mutable-individual-suitability.model';
+import { HasAccessToCamera, Hearing, SuitabilityAnswer } from '../../base-journey/participant-suitability.model';
+
+describe('representative.journey.service', () => {
+  let individualJourneyService: IndividualJourneyService;
+
+  beforeEach(() => {
+    individualJourneyService = new IndividualJourneyService();
+  });
+
+  it('returns null when item not exist', () => {
+    expect(individualJourneyService.get()).toBeNull();
+  });
+
+  it('returns the model from storage', () => {
+    const model = new MutableIndividualSuitabilityModel();
+    model.hearing = new Hearing('hearing1', new Date(2012, 12, 12, 12, 15, 11, 23));
+    model.aboutYou = new SuitabilityAnswer();
+    model.aboutYou.answer = true;
+    model.aboutYou.notes = 'true';
+    model.consent = new SuitabilityAnswer();
+    model.consent.answer = true;
+    model.consent.notes = 'true';
+    model.internet = true;
+    model.interpreter = true;
+    model.room = true;
+    model.camera = HasAccessToCamera.NotSure;
+    model.computer = true;
+
+    individualJourneyService.set(model);
+
+    const result = individualJourneyService.get();
+
+    expect(JSON.stringify(result)).toBe(JSON.stringify(model));
+  });
+});
