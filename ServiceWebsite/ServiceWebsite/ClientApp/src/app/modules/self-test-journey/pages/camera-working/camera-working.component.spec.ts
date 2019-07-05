772d8013d16da95e6bc94fb10c9631f706d40f54
kkodalikk <karuna.kodali@hmcts.net>
2019-07-05 15:37:43 +0100

    Added Equipment Check buttons

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/camera-working/camera-working.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/camera-working/camera-working.component.spec.ts
@@ -6,18 +6,32 @@ import {
 import {ContinuableComponentFixture} from '../../../base-journey/components/suitability-choice-component-fixture.spec';
 import {CameraWorkingComponent} from './camera-working.component';
 import { SelfTestJourneySteps } from '../../self-test-journey-steps';
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 describe('CameraWorkingComponent', () => {
-  it(`goes to ${SelfTestJourneySteps.SeeAndHearVideo} on continuing`, () => {
-    const journey = jasmine.createSpyObj<JourneyBase>(['goto']);
-    const fixture = SelfTestJourneyComponentTestBed.createComponent({
+  let fixture: ComponentFixture<CameraWorkingComponent>;
+  let journey: jasmine.SpyObj<JourneyBase>;
+
+  beforeEach(() => {
+    journey = jasmine.createSpyObj<JourneyBase>(['goto']);
+    fixture = SelfTestJourneyComponentTestBed.createComponent({
       component: CameraWorkingComponent,
       declarations: [CrestBluePanelComponent],
       journey: journey
     });
+  });
 
+  it(`goes to ${SelfTestJourneySteps.SeeAndHearVideo} on continuing`, () => {
     fixture.detectChanges();
     new ContinuableComponentFixture(fixture).submitIsClicked();
     expect(journey.goto).toHaveBeenCalledWith(SelfTestJourneySteps.SeeAndHearVideo);
+  });
+
+  it(`redirects to ${SelfTestJourneySteps.SelfTest} on clicking on check your equipment again`,  () => {
+    fixture.detectChanges();
+    const checkYourEquipmentButton = fixture.debugElement.query(By.css('#checkYourEquipment'));
+    checkYourEquipmentButton.nativeElement.click();
+    expect(journey.goto).toHaveBeenCalledWith(SelfTestJourneySteps.SelfTest);
   });
 });
