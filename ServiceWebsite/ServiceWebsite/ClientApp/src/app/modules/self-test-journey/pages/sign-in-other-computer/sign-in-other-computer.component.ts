62b32df20ad78926ce64a4b508b695ebb6fbb825
David <ddikman@gmail.com>
2019-07-05 13:54:24 +0100

    Fixed test failure

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/sign-in-other-computer/sign-in-other-computer.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/sign-in-other-computer/sign-in-other-computer.component.ts
@@ -1,23 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {
-  SuitabilityChoicePageBaseComponent as GenericSuitabilityChoicePageBaseComponent
-} from '../../../base-journey/components/suitability-choice-page-base.component';
-import {JourneyBase} from '../../../base-journey/journey-base';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in-other-computer',
-  templateUrl: './sign-in-other-computer.component.html',
-  styles: []
+  templateUrl: './sign-in-other-computer.component.html'
 })
-export class SignInOtherComputerComponent extends GenericSuitabilityChoicePageBaseComponent<JourneyBase> implements OnInit {
-
-  constructor(journey: JourneyBase) {
-    super(journey);
-  }
-
-  ngOnInit(): void {
-  }
-
-  protected bindModel(): void {
-  }
-}
+export class SignInOtherComputerComponent { }
