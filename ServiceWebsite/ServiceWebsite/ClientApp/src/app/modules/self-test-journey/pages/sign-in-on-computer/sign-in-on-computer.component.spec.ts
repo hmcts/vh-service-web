eb4166946fcccd4d0ae172fd1b88a2f1614327de
atif.rehman <atif.rehman@hmcts.net>
2019-07-09 12:13:24 +0100

    VIH-4577: Adding inital sign_in_on_computer page when user is on mobile

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/sign-in-on-computer/sign-in-on-computer.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/sign-in-on-computer/sign-in-on-computer.component.spec.ts
@@ -1,19 +1,19 @@
 import { ContactUsComponent } from '../../../shared/contact-us/contact-us.component';
 import { CrestBluePanelComponent } from '../../../shared/crest-blue-panel/crest-blue-panel.component';
-import { SignInOtherComputerComponent } from './sign-in-other-computer.component';
+import { SignInOnComputerComponent } from './sign-in-on-computer.component';
 import { TestBed } from '@angular/core/testing';
 
-describe('SignInOtherComputerComponent', () => {
+describe('SignInOnComputerComponent', () => {
   it('can create component', () => {
     TestBed.configureTestingModule({
       declarations: [
         CrestBluePanelComponent,
-        SignInOtherComputerComponent,
+        SignInOnComputerComponent,
         ContactUsComponent
       ],
     }).compileComponents();
 
-    const fixture = TestBed.createComponent(SignInOtherComputerComponent);
+    const fixture = TestBed.createComponent(SignInOnComputerComponent);
     fixture.detectChanges();
     expect(fixture.componentInstance).toBeTruthy();
   });
