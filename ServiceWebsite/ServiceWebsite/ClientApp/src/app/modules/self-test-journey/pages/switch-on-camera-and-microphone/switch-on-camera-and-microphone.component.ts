fde571e230ed6e42f20ed8ae4e3311295aca7a54
kkodalikk <karuna.kodali@hmcts.net>
2019-07-11 10:25:31 +0100

    Changes to Switch on Camera page

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/switch-on-camera-and-microphone/switch-on-camera-and-microphone.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/switch-on-camera-and-microphone/switch-on-camera-and-microphone.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {JourneyBase} from '../../../base-journey/journey-base';
 import { SelfTestJourneySteps } from '../../self-test-journey-steps';
+import { MediaService } from 'src/app/modules/individual-journey/services/media.service';
+import { ParticipantSuitabilityModel } from 'src/app/modules/base-journey/participant-suitability.model';
 
 @Component({
   selector: 'app-switch-on-camera-and-microphone',
@@ -9,14 +11,19 @@ import { SelfTestJourneySteps } from '../../self-test-journey-steps';
 })
 export class SwitchOnCameraAndMicrophoneComponent implements OnInit {
 
-  constructor(private journey: JourneyBase) {
+  constructor(private journey: JourneyBase, private mediaAccess: MediaService, private model: ParticipantSuitabilityModel) {
 
   }
 
   ngOnInit(): void {
   }
 
-  switchOnCameraAndMicrophone(): void {
+  switchOnCameraAndMicrophone(): Promise<void> {
+    this.mediaAccepted = await this.mediaAccess.requestAccess();
+    if (!this.mediaAccepted) {
+      this.model.mediaAccepted = false;
+      this.journey.goto(IndividualJourneySteps.MediaAccessError);
+    }
   }
 
   continue() {
