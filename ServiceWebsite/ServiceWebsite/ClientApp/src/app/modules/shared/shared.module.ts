1b063c03dcdcc7ef9e08e65d71b5a158f215bbb6
David <ddikman@gmail.com>
2019-07-12 11:51:43 +0100

    Lifted all the packages and dependencies

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/shared/shared.module.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DeviceDetectorModule } from 'ngx-device-detector';
 
 // components
@@ -26,7 +25,6 @@ import { AppYesNoPipe } from './boolean.pipe';
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    NgbModule.forRoot(),
     DeviceDetectorModule.forRoot(),
   ],
   declarations: [
