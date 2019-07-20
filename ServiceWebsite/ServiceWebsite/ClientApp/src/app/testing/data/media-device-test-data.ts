e1af201336cb7f9584cb6d791943bded01dcffae
Valentina <Valentina.bennett@hmcts.net>
2019-07-20 12:16:24 +0100

    Added user media service

---
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/testing/data/media-device-test-data.ts
@@ -0,0 +1,54 @@
+import { UserMediaDevice } from '../../modules/self-test-journey/models/user-media-device';
+
+export class MediaDeviceTestData {
+  getListOfDevices() {
+    let testData: Array<UserMediaDevice> = [];
+    testData = testData.concat(this.getListOfCameras());
+    testData = testData.concat(this.getListOfMicrophones());
+    const defaultDevice = new UserMediaDevice('camera1', 'default', 'videoinput', 'group1');
+    const soundOutput = new UserMediaDevice('audiooutput1', 'audiooutput1', 'audiooutput', 'group1');
+
+    testData.push(defaultDevice);
+    testData.push(soundOutput);
+    return testData;
+  }
+
+  getListOfSingleCameraAndMicDevices() {
+    const testData: Array<UserMediaDevice> = [];
+    testData.concat(this.getSingleCamera());
+    testData.concat(this.getSingleMicrophone());
+    return testData;
+  }
+
+  getSingleCamera(): UserMediaDevice[] {
+    const testData: UserMediaDevice[] = [];
+    const device = new UserMediaDevice('camera1', 'camId1', 'videoinput', 'group1');
+    testData.push(device);
+    return testData;
+  }
+
+  getListOfCameras(): UserMediaDevice[] {
+    const testData: UserMediaDevice[] = [];
+    const device = new UserMediaDevice('camera1', 'camId1', 'videoinput', 'group1');
+    const device2 = new UserMediaDevice('camera2', 'camId2', 'videoinput', 'group2');
+    testData.push(device);
+    testData.push(device2);
+    return testData;
+  }
+
+  getSingleMicrophone(): UserMediaDevice[] {
+    const testData: UserMediaDevice[] = [];
+    const device = new UserMediaDevice('mic1', 'micId1', 'audioinput', 'group1');
+    testData.push(device);
+    return testData;
+  }
+
+  getListOfMicrophones(): UserMediaDevice[] {
+    const testData: UserMediaDevice[] = [];
+    const device = new UserMediaDevice('mic1', 'micId1', 'audioinput', 'group1');
+    const device2 = new UserMediaDevice('mic2', 'micId2', 'audioinput', 'group2');
+    testData.push(device);
+    testData.push(device2);
+    return testData;
+  }
+}
