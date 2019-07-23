7667e5087383b08b2274d19ef38fe9d66cc48ea6
atif.rehman <atif.rehman@hmcts.net>
2019-07-23 16:44:21 +0100

    VIH-4718: Change BFF endpoint for GetParticipantsByUsername so no username passed in [HttpGet("participants/{username}")]

--- ServiceWebsite/ServiceWebsite/Security/HashGen/CustomTokenSettings.cs
+++ ServiceWebsite/ServiceWebsite/Security/HashGen/CustomTokenSettings.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ServiceWebsite.Security.HashGen
+﻿namespace ServiceWebsite.Security.HashGen
 {
     public class CustomTokenSettings
     {
