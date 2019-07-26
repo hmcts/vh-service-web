76ff765e52b52c1148a5b1b8782801a6412d9bd1
Atif Rehman <atif.rehman@hmcts.net>
2019-07-26 17:13:23 +0100

    VIH-3942: Adding tests

---
+++ ServiceWebsite/ServiceWebsite.UnitTests/Services/KinlyPlatformServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using ServiceWebsite.Common.Security;
+using ServiceWebsite.Services;
+
+namespace ServiceWebsite.UnitTests.Services
+{
+    [TestFixture]
+    public class KinlyPlatformServiceTest
+    {
+        private Mock<ICustomJwtTokenProvider> _CustomJwtTokenProvider;
+
+        private KinlyPlatformService _kinlyPlatformService;
+
+        public KinlyPlatformServiceTest()
+        {
+            _CustomJwtTokenProvider = new Mock<ICustomJwtTokenProvider>();
+
+            _kinlyPlatformService = new KinlyPlatformService(_CustomJwtTokenProvider.Object, "url");
+        }
+
+        [Test]
+        public async Task GetTestCallScoreAsync_()
+        {
+
+        }
+    }
+}
