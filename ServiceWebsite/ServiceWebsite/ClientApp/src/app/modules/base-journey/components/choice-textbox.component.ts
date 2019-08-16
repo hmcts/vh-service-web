e62613da5e077b5619eab484f80d15ecb9cac42b
Atif Rehman <atif.rehman@hmcts.net>
2019-08-16 15:00:59 +0100

    VIH-4872: change text

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/components/choice-textbox.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/components/choice-textbox.component.ts
@@ -1,30 +1,30 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { ChoiceTextboxForm } from './choice-textbox-form';
-import { SuitabilityAnswer } from '../participant-suitability.model';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {ChoiceTextboxForm} from './choice-textbox-form';
+import {SuitabilityAnswer} from '../participant-suitability.model';
 
 @Component({
-    selector: 'app-choice-textbox',
-    templateUrl: './choice-textbox.component.html'
-  })
-  export class ChoiceTextboxComponent implements OnInit {
+  selector: 'app-choice-textbox',
+  templateUrl: './choice-textbox.component.html'
+})
+export class ChoiceTextboxComponent implements OnInit {
 
-    @Input()
-    suitabilityAnswer: SuitabilityAnswer;
+  @Input()
+  suitabilityAnswer: SuitabilityAnswer;
 
-    @Output()
-    submitted = new EventEmitter();
+  @Output()
+  submitted = new EventEmitter();
 
-    readonly form = new ChoiceTextboxForm();
+  readonly form = new ChoiceTextboxForm();
 
-    ngOnInit() {
-      this.form.submitted.subscribe(() => this.onSubmitted());
-      this.form.choice.setValue(this.suitabilityAnswer.answer);
-      this.form.textInput.setValue(this.suitabilityAnswer.notes);
-    }
-
-    protected onSubmitted(): void {
-      this.suitabilityAnswer.answer = this.form.choice.value;
-      this.suitabilityAnswer.notes = this.form.choice.value ? this.form.textInput.value : null;
-      this.submitted.emit();
-    }
+  ngOnInit() {
+    this.form.submitted.subscribe(() => this.onSubmitted());
+    this.form.choice.setValue(this.suitabilityAnswer.answer);
+    this.form.textInput.setValue(this.suitabilityAnswer.notes);
   }
+
+  protected onSubmitted(): void {
+    this.suitabilityAnswer.answer = this.form.choice.value;
+    this.suitabilityAnswer.notes = this.form.choice.value ? this.form.textInput.value : null;
+    this.submitted.emit();
+  }
+}
