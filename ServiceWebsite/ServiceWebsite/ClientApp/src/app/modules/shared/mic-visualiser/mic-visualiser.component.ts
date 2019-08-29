012c705cfacb4d8d63d4d727ef69a57e2b177a76
Sunil DSouza <sunil.dsouza@hmcts.net>
2019-08-29 15:45:39 +0100

    Fixinf linting issue

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/shared/mic-visualiser/mic-visualiser.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/shared/mic-visualiser/mic-visualiser.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import 'webrtc-adapter';
 
 @Component({
   selector: 'app-mic-visualiser',
   templateUrl: './mic-visualiser.component.html'
 })
-export class MicVisualiserComponent implements OnInit {
+export class MicVisualiserComponent implements OnInit, OnDestroy {
 
   canvasContext: CanvasRenderingContext2D;
   audioContext: AudioContext;
