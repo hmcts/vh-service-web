0806b7964df9b66facb8ca26045a35b6873e225d
Valentina <Valentina.bennett@hmcts.net>
2019-09-27 15:51:49 +0100

    refactoring

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/detect-i-pad.service.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/detect-i-pad.service.spec.ts
@@ -9,30 +9,12 @@ describe('DetectIPadService', () => {
     userAgent = 'desktop';
   }
 
-  class Window1 {
-    constructor(h, w) {
-      this.screen.height = h;
-      this.screen.width = w;
-    }
-    screen = {
-      height: 0,
-      width: 0
-    };
-  }
-
   it('detect iPad return true', () => {
     service._navigator = new Agent();
-    service._window = new Window1(1024, 768);
     expect(service.detectiPad()).toBeTruthy();
   });
   it('detect iPad return false', () => {
     service._navigator = new Agent1();
-    service._window = new Window1(5024, 768);
     expect(service.detectiPad()).toBeFalsy();
-  });
-  it('detect iPad if size or model is match iPad', () => {
-    service._navigator = new Agent();
-    service._window = new Window1(5024, 768);
-    expect(service.detectiPad()).toBeTruthy();
   });
 });
--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/detect-i-pad.service.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/detect-i-pad.service.ts
@@ -6,20 +6,11 @@ import { window } from 'rxjs/operators';
 })
 export class DetectIPadService {
   _navigator = <any>navigator;
-  _window = <any>window;
 
   detectiPad(): boolean {
-    return this.detectByAgentiPad() || this.detectByScreeniPad();
+    return this.detectByAgentiPad();
   }
-
-  private detectByScreeniPad() {
-    // iPad, iPad 2, iPad Mini, iPad 3, 4, 5, Mini 2, Mini 3, Mini 4, Air, Air 2, Pro 9.7
-    // iPad Pro 10.5 or iPad Pro 12.9, Pro 12.9 (2nd Gen)
-    return ((this._window.screen.height / this._window.screen.width === 1024 / 768) ||
-      (this._window.screen.height / this._window.screen.width === 1112 / 834) ||
-      (this._window.screen.height / this._window.screen.width === 1366 / 1024));
-  }
-
+  
   private detectByAgentiPad() {
     if (this._navigator && this._navigator.userAgent && this._navigator.userAgent != null) {
       const strUserAgent = this._navigator.userAgent.toLowerCase();
