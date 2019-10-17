a6cbccdf13b31ab0414ec7968b3e1c59165dc52f
Valentina <Valentina.bennett@hmcts.net>
2019-10-17 14:29:43 +0100

    refactoring unit tests

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/explore-court-building/explore-court-building.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/explore-court-building/explore-court-building.component.spec.ts
@@ -2,14 +2,19 @@ import { IndividualJourneySteps } from './../../individual-journey-steps';
 import { CommonIndividualComponentTests } from '../individual-base-component/individual-component-test-bed.spec';
 import { ExploreCourtBuildingComponent } from './explore-court-building.component';
 import { BackNavigationStubComponent } from '../../../../testing/stubs/back-navigation-stub';
+import { DeviceType } from 'src/app/modules/base-journey/services/device-type';
 
 describe('ExploreCourtBuildingComponent', () => {
+  let deviceType: jasmine.SpyObj<DeviceType>;
+  deviceType = jasmine.createSpyObj<DeviceType>(['isMobile', 'isTablet']);
+
   it(`goes to ${IndividualJourneySteps.CourtInformationVideo} when pressing continue`, () => {
     CommonIndividualComponentTests.goesToStepWhenButtonIsPressed(
       IndividualJourneySteps.CourtInformationVideo,
       {
         component: ExploreCourtBuildingComponent,
-        declarations: [BackNavigationStubComponent]
+        declarations: [BackNavigationStubComponent],
+        providers: [{ provide: DeviceType, useValue: deviceType }]
       }
     );
   });
--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/sign-in-on-computer/sign-in-on-computer.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/sign-in-on-computer/sign-in-on-computer.component.spec.ts
@@ -1,5 +1,5 @@
-import { ContactUsComponent } from '../../../shared/contact-us/contact-us.component';
-import { CrestBluePanelComponent } from '../../../shared/crest-blue-panel/crest-blue-panel.component';
+import { ContactUsComponent } from '../../modules/shared/contact-us/contact-us.component';
+import { CrestBluePanelComponent } from '../../modules/shared/crest-blue-panel/crest-blue-panel.component';
 import { SignInOnComputerComponent } from './sign-in-on-computer.component';
 import { TestBed } from '@angular/core/testing';
 
