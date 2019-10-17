ef82b745d504a70432e2b485ae61e44bd5d506c1
Valentina <Valentina.bennett@hmcts.net>
2019-10-17 10:34:22 +0100

    made common the sign in on computer component

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/explore-court-building/explore-court-building.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/pages/explore-court-building/explore-court-building.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IndividualJourney } from '../../individual-journey';
 import { IndividualJourneySteps } from '../../individual-journey-steps';
+import { DeviceType } from 'src/app/modules/base-journey/services/device-type';
 
 @Component({
   selector: 'app-explore-court-building',
@@ -8,9 +9,13 @@ import { IndividualJourneySteps } from '../../individual-journey-steps';
   styles: []
 })
 export class ExploreCourtBuildingComponent {
-  constructor(private journey: IndividualJourney) {}
+  constructor(private journey: IndividualJourney, private deviceType: DeviceType) {}
 
   continue() {
+    if (this.deviceType.isMobile() || this.deviceType.isTablet()) {
+      this.journey.goto(IndividualJourneySteps.SignInOnComputer);
+      return;
+    }
     this.journey.goto(IndividualJourneySteps.CourtInformationVideo);
   }
 }
---
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/sign-in-on-computer/sign-in-on-computer.component.spec.ts
@@ -0,0 +1,20 @@
+import { ContactUsComponent } from '../../../shared/contact-us/contact-us.component';
+import { CrestBluePanelComponent } from '../../../shared/crest-blue-panel/crest-blue-panel.component';
+import { SignInOnComputerComponent } from './sign-in-on-computer.component';
+import { TestBed } from '@angular/core/testing';
+
+describe('SignInOnComputerComponent', () => {
+  it('can create component', () => {
+    TestBed.configureTestingModule({
+      declarations: [
+        CrestBluePanelComponent,
+        SignInOnComputerComponent,
+        ContactUsComponent
+      ],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(SignInOnComputerComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+});
