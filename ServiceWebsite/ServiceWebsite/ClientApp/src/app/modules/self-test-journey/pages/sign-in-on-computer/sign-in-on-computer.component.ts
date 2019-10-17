f7662010ac41cdc84bfc44c8b1e66a0d158dc2ff
Valentina <Valentina.bennett@hmcts.net>
2019-10-17 11:43:22 +0100

    remove sign in component from self-test

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/self-test-journey/pages/sign-in-on-computer/sign-in-on-computer.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sign-in-on-computer',
-  templateUrl: './sign-in-on-computer.component.html'
-})
-export class SignInOnComputerComponent { }
