ff0dd6e729909c9c224fba6f1903548f11c4c9fe
Valentina <Valentina.bennett@hmcts.net>
2019-10-29 09:10:50 +0000

    Setup azure media played, get video from AzureMedia Service

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/video-view/video-view.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/video-view/video-view.component.ts
@@ -8,56 +8,63 @@ Muted videos can autoplay in Chrome. Autoplay with sound is allowed if:
   3)On mobile, the user has [added the site to their home screen].
  */
 @Component({
-  selector: 'app-video-view',
-  templateUrl: './video-view.component.html',
-  styles: []
+    selector: 'app-video-view',
+    templateUrl: './video-view.component.html',
+    styles: []
 })
 export class VideoViewComponent implements OnDestroy {
-  constructor(private logger: Logger) {
-  }
+    constructor(private logger: Logger) {
+    }
+
+  //from the doc example
+  //currentMovie = 'https://amssamples.streaming.mediaservices.windows.net/3b970ae0-39d5-44bd-b3a3-3136143d6435/AzureMediaServicesPromo.ism/manifest';
+
+  //url from the azure media service
+  currentMovie = 'http://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.ism/manifest';
+
   @Output()
-  loaded: EventEmitter<any> = new EventEmitter<any>();
+    loaded: EventEmitter<any> = new EventEmitter<any>();
 
-  @Input()
-  source: string;
+    @Input()
+    source: string;
 
-  @ViewChild('video', { static: false })
-  videoElement: ElementRef;
+    @ViewChild('video', { static: false })
+    videoElement: ElementRef;
 
-  videoUnavailable: boolean;
-  /**
-   * This method is invoked when a video is ready to start playing
-   */
-  readyToPlay() {
-    this.loaded.emit();
-  }
+    videoUnavailable: boolean;
+    /**
+     * This method is invoked when a video is ready to start playing
+     */
+    readyToPlay() {
+        this.loaded.emit();
+    }
 
-  onVideoError(): void {
-    this.videoUnavailable = true;
-    this.logger.error('The video file is not available.', null, null);
-  }
-  /**
-   * Plays the video
-   */
-  async play() {
-    this.stopPlaying();
-    this.videoTag.muted = false;
-    await this.videoTag.play();
-  }
+    onVideoError(): void {
+        this.videoUnavailable = true;
+        this.logger.error('The video file is not available.', null, null);
+    }
+    /**
+     * Plays the video
+     */
+    async play() {
+        this.stopPlaying();
+        this.videoTag.muted = false;
+        await this.videoTag.play();
+    }
 
-  private get videoTag() {
-    const video: HTMLVideoElement = this.videoElement.nativeElement;
-    return video;
-  }
+    private get videoTag() {
+        const video: HTMLVideoElement = this.videoElement.nativeElement;
+        return video;
+    }
 
-  private stopPlaying() {
-    const videoElement = this.videoTag;
-    videoElement.pause();
-    videoElement.currentTime = 0;
-    videoElement.muted = true;
-  }
+    private stopPlaying() {
+        const videoElement = this.videoTag;
+        videoElement.pause();
+        videoElement.currentTime = 0;
+        videoElement.muted = true;
+    }
 
-  ngOnDestroy() {
-    this.stopPlaying();
-  }
+    ngOnDestroy() {
+        this.stopPlaying();
+    }
 }
