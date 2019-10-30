3fada948790fc3b834e5a672b294fa7af607eb1a
kkodalikk <45096345+kkodalikk@users.noreply.github.com>
2019-10-30 10:59:13 +0000

    Removed Support for Edge

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/device-type.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/device-type.ts
@@ -23,7 +23,7 @@ export class DeviceType {
   }
 
   isSupportedBrowser(): boolean {
-    const supportedBrowsers = ['Firefox', 'Safari', 'Chrome', 'Edge', 'MS-Edge'];
+    const supportedBrowsers = ['Firefox', 'Safari', 'Chrome'];
     const browser = this.deviceDetectorService.browser;
     return supportedBrowsers.findIndex(x => x.toUpperCase() === browser.toUpperCase()) > -1;
   }
