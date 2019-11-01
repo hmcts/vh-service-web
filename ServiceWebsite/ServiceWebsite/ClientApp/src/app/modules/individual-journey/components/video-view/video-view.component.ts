31f8cf2becaa07f7c02131fbb40f038006824113
Valentina <Valentina.bennett@hmcts.net>
2019-11-01 10:15:28 +0000

    testing video media service

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/video-view/video-view.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/individual-journey/components/video-view/video-view.component.ts
@@ -17,11 +17,14 @@ export class VideoViewComponent implements OnDestroy {
     }
 
   //from the doc example
-  //currentMovie = 'https://amssamples.streaming.mediaservices.windows.net/3b970ae0-39d5-44bd-b3a3-3136143d6435/AzureMediaServicesPromo.ism/manifest';
+ // currentMovie = 'https://amssamples.streaming.mediaservices.windows.net/3b970ae0-39d5-44bd-b3a3-3136143d6435/AzureMediaServicesPromo.ism/manifest';
 
   //url from the azure media service
-  currentMovie = 'http://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.ism/manifest';
+  //currentMovie = 'https://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_court.ism/manifest';
 
+  currentMovie = 'http://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.ism/manifest';
+  captionFile = 'http://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.mp4.ttml';
+  
   @Output()
     loaded: EventEmitter<any> = new EventEmitter<any>();
 
--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/accessibility/accessibility.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/accessibility/accessibility.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./accessibility.component.css']
 })
 export class AccessibilityComponent {
+ currentMovie = 'https://amssamples.streaming.mediaservices.windows.net/3b970ae0-39d5-44bd-b3a3-3136143d6435/AzureMediaServicesPromo.ism/manifest';
+
+  //url from the azure media service
+  //currentMovie = 'http://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.ism/manifest';
+  //currentMovie = 'https://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_court.ism/manifest';
+
+  captionFile = 'http://vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.mp4.ttml';
+
 
   isVisibleContents = true;
   isFooter = false;
