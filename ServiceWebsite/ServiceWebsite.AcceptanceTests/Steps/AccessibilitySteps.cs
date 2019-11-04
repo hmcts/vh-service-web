80356da5071661c51ba779f6b2de186eb212da5d
Andressa Dantas <Andressa.DeAzevedoDantas@HMCTS.NET>
2019-11-04 16:38:31 +0000

    Removed excluded rules from analysis.

--- ServiceWebsite/ServiceWebsite.AcceptanceTests/Steps/AccessibilitySteps.cs
+++ ServiceWebsite/ServiceWebsite.AcceptanceTests/Steps/AccessibilitySteps.cs
@@ -1,5 +1,4 @@
 ﻿using ServiceWebsite.AcceptanceTests.Helpers;
-using ServiceWebsite.AcceptanceTests.NuGet.Contexts;
 using TechTalk.SpecFlow;
 using Selenium.Axe;
 using FluentAssertions;
@@ -19,14 +18,7 @@ namespace ServiceWebsite.AcceptanceTests.Steps
         [Then(@"the page should be accessible")]
         public void ThenThePageShouldBeAccessible()
         {
-            var axeResult = new AxeBuilder(_browserContext.NgDriver).DisableRules( // BUG: Once VIH-5174 bug is fixed, remove these exclusions
-                    "region", // https://dequeuniversity.com/rules/axe/3.3/region?application=axeAPI
-                    "landmark-one-main", // https://dequeuniversity.com/rules/axe/3.3/landmark-one-main?application=axeAPI
-                    "landmark-no-duplicate-banner", // https://dequeuniversity.com/rules/axe/3.3/landmark-no-duplicate-banner?application=axeAPI
-                    "landmark-no-duplicate-contentinfo", // https://dequeuniversity.com/rules/axe/3.3/landmark-no-duplicate-contentinfo?application=axeAPI
-                    "page-has-heading-one", // https://dequeuniversity.com/rules/axe/3.3/page-has-heading-one?application=axeAPI
-                    "landmark-unique") // https://dequeuniversity.com/rules/axe/3.3/landmark-unique?application=axeAPI
-                .Analyze();
+            var axeResult = new AxeBuilder(_browserContext.NgDriver).Analyze();
             axeResult.Violations.Should().BeEmpty();
         }
     }
