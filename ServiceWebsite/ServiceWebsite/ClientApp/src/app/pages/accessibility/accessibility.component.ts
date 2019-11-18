ae00164f8a8d28df3a3a3e07c275e6c9d5b04db1
Valentina <Valentina.bennett@hmcts.net>
2019-11-18 10:45:02 +0000

    refactorign

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/accessibility/accessibility.component.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/pages/accessibility/accessibility.component.ts
@@ -6,13 +6,15 @@ import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
   styleUrls: ['./accessibility.component.css']
 })
 export class AccessibilityComponent implements AfterViewInit {
+  srcProgress = 'https://vhcoreinfratest1.blob.core.windows.net/asset-cbcd4569-3775-4994-91a8-a1e019de17e1/btd_individual_laptop.mp4.vtt?sv=2017-04-17&sr=c&si=e30e8eff-b0bb-435e-b8ce-1a4cefea0cba&sig=jP125rTmY8dV0TjnrMUWoVejrI0qx9TnNdA17PS12mc%3D&st=2019-11-05T10%3A45%3A19Z&se=2119-11-12T10%3A45%3A19Z';
 
   srcLama = '//amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism';
   srcEx1 = "//amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest";
 
   srcVideo = '//vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.ism/manifest';
   srcCourt = '//vhcoreinfratest1-ukwe1.streaming.media.azure.net/41da90e5-8689-4ffb-b86d-616d0a71c5e9/btd_court.ism/manifest';
-
+  srcIndivd =
+    "//vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.ism/manifest";
   srcMp4 = '//vhcoreinfrapreview.blob.core.windows.net/video/btd_court.mp4';
   captionFile = '//vhcoreinfratest1-ukwe1.streaming.media.azure.net/5b20f544-272c-45a1-a7b6-52b03e99abae/btd_individual_laptop.mp4.ttml';
 
@@ -30,14 +32,12 @@ export class AccessibilityComponent implements AfterViewInit {
     width: "640",
     height: "400",
     id: 'vh-vd1',
-    logo: { enabled: true },
+    logo: { enabled: false },
   };
 
   ampPlayer: any;
 
-
   constructor() {
-
   }
 
   ngAfterViewInit() {
@@ -46,15 +46,14 @@ export class AccessibilityComponent implements AfterViewInit {
       this.ampPlayer = amp;
 
       this.ampPlayer.src([{
-        src: this.srcVideo,
+        src: this.srcIndivd,
         type: 'application/vnd.ms-sstr+xml',
         
       }],
         [
           {
-            src:
-              "https://vhcoreinfratest1.blob.core.windows.net/asset-cbcd4569-3775-4994-91a8-a1e019de17e1/btd_individual_laptop.mp4.vtt?sv=2017-04-17&sr=c&si=e30e8eff-b0bb-435e-b8ce-1a4cefea0cba&sig=jP125rTmY8dV0TjnrMUWoVejrI0qx9TnNdA17PS12mc%3D&st=2019-11-05T10%3A45%3A19Z&se=2119-11-12T10%3A45%3A19Z";
-            srclang: "en",
+            src: this.captionFile,
+            lang: "en",
             label: "track1",
             kind: "captions"
           }
