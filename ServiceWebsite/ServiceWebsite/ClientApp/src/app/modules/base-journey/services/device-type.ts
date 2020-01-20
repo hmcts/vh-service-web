0f6b54470cc79a880515421d8a265d3e78848da3
kkodalikk <45096345+kkodalikk@users.noreply.github.com>
2020-01-20 15:31:34 +0000

    Added Edge to the Supported browser list

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/device-type.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/services/device-type.ts
@@ -23,7 +23,7 @@ export class DeviceType {
   }
 
   isSupportedBrowser(): boolean {
-    const supportedBrowsers = ['Firefox', 'Safari', 'Chrome', 'MS-Edge'];
+      const supportedBrowsers = ['Firefox', 'Safari', 'Chrome', 'Edge', 'MS-Edge'];
     const browser = this.deviceDetectorService.browser;
     return supportedBrowsers.findIndex(x => x.toUpperCase() === browser.toUpperCase()) > -1;
   }
