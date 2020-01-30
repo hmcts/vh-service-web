c16c2480368c0b8a7b2084e8b08c2f4ed6a321f7
Lionel Roche <lionel.roche@hmcts.net>
2020-01-30 13:10:45 +0000

    lint fix: moved variable definition to top of class

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/journey-base.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/base-journey/journey-base.ts
@@ -3,7 +3,7 @@ import { JourneyStep } from './journey-step';
 
 export abstract class JourneyBase {
     readonly redirect: EventEmitter<JourneyStep>;
-
+    abstract journeyName: string;
     /**
     * Sets the journey to a specific step. This can be used when navigating to a specific step in the journey.
     * @param position The step to jump to
@@ -15,6 +15,4 @@ export abstract class JourneyBase {
     abstract goto(step: JourneyStep): void;
 
     abstract submitQuestionnaire(): Promise<void>;
-
-    abstract journeyName: string;
 }
