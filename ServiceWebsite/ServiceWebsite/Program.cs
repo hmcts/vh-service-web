d0a4bdc78ef8c1b76af8dae227968900fd2fe952
Ryan Bartram <ryan@dftai.ch>
2020-02-07 10:32:02 +0000

    fix formatting to match rest of project

--- ServiceWebsite/ServiceWebsite/Program.cs
+++ ServiceWebsite/ServiceWebsite/Program.cs
@@ -3,25 +3,29 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ServiceWebsite {
-    public static class Program {
-        public static void Main (string[] args) {
-            CreateWebHostBuilder (args).Build ().Run ();
+namespace ServiceWebsite
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        private static IHostBuilder CreateWebHostBuilder (string[] args) {
-            return Host.CreateDefaultBuilder (args)
-                .ConfigureWebHostDefaults (webBuilder => {
-                    webBuilder.UseContentRoot (Directory.GetCurrentDirectory ());
-                    webBuilder.UseIISIntegration ();
-                    webBuilder.UseStartup<Startup> ();
-                    webBuilder.ConfigureLogging ((hostingContext, logging) => {
-                        logging.AddEventSourceLogger ();
-                        logging
-                            .AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>
-                            ("", LogLevel.Trace);
-                    });
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                webBuilder.UseIISIntegration();
+                webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddEventSourceLogger();
+                    logging
+                        .AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>
+                        ("", LogLevel.Trace);
                 });
-        }
+            });
     }
 }
--- ServiceWebsite/ServiceWebsite/Startup.cs
+++ ServiceWebsite/ServiceWebsite/Startup.cs
@@ -16,9 +16,12 @@ using ServiceWebsite.Configuration;
 using ServiceWebsite.Controllers;
 using ServiceWebsite.Helpers;
 
-namespace ServiceWebsite {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace ServiceWebsite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
@@ -28,64 +31,75 @@ namespace ServiceWebsite {
         ///     This method gets called by the runtime. Use this method to add services to the container.
         /// </summary>
         /// <param name="services"></param>
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddSingleton<ITelemetryInitializer> (new CloudRoleNameInitializer ());
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITelemetryInitializer>(new CloudRoleNameInitializer());
 
-            services.AddCors (options => options.AddPolicy ("CorsPolicy",
-                builder => {
+            services.AddCors(options => options.AddPolicy("CorsPolicy",
+                builder =>
+                {
                     builder
-                        .AllowAnyHeader ()
-                        .AllowAnyMethod ()
-                        .SetIsOriginAllowed ((host) => true)
-                        .AllowCredentials ();
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .SetIsOriginAllowed((host) => true)
+                        .AllowCredentials();
                 }));
 
-            services.AddJsonOptions ();
+            services.AddJsonOptions();
 
-            RegisterSettings (services);
+            RegisterSettings(services);
 
-            services.AddCustomTypes ();
+            services.AddCustomTypes();
 
-            var settings = Configuration.GetSection ("AzureAd").Get<SecuritySettings> ();
-            services.AddApplicationInsightsTelemetry (settings.AppInsightsKey);
+            var settings = Configuration.Get<EnvironmentSettings>();
+            services.AddApplicationInsightsTelemetry(settings.AppInsightsKey);
 
-            RegisterAuth (services);
-            services.AddMvc (options => options.EnableEndpointRouting = false);
+            RegisterAuth(services);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
 
-            services.AddSpaStaticFiles (configuration => {
+            services.AddSpaStaticFiles(configuration =>
+            {
                 configuration.RootPath = "ClientApp/dist";
             });
         }
 
-        private void RegisterSettings (IServiceCollection services) {
-            services.Configure<SecuritySettings> (options => Configuration.Bind ("AzureAd", options));
-            services.Configure<ServiceSettings> (options => Configuration.Bind ("VhServices", options));
-            services.Configure<AppConfigSettings> (options => Configuration.Bind (options));
+        private void RegisterSettings(IServiceCollection services)
+        {
+            services.Configure<SecuritySettings>(options => Configuration.Bind("AzureAd", options));
+            services.Configure<ServiceSettings>(options => Configuration.Bind("VhServices", options));
+            services.Configure<AppConfigSettings>(options => Configuration.Bind(options));
+            services.Configure<SecuritySettings>(options => Configuration.Bind("ApplicationInsights", options));
+            services.Configure<EnvironmentSettings>(options => Configuration.Bind(options));
+            services.Configure<AppConfigSettings>(options => Configuration.Bind(options));
+            services.Configure<EnvironmentSettings>(options => Configuration.Bind("AzureStorage", options));
 
-            var customTokenSettings = Configuration.GetSection ("CustomToken").Get<CustomTokenSettings> ();
-            services.AddSingleton (customTokenSettings);
+            var customTokenSettings = Configuration.GetSection("CustomToken").Get<CustomTokenSettings>();
+            services.AddSingleton(customTokenSettings);
         }
 
-        private void RegisterAuth (IServiceCollection services) {
-            var securitySettings = Configuration.GetSection ("AzureAd").Get<SecuritySettings> ();
+        private void RegisterAuth(IServiceCollection services)
+        {
+            var settings = Configuration.Get<EnvironmentSettings>();
 
-            var policy = new AuthorizationPolicyBuilder ()
-                .RequireAuthenticatedUser ()
-                .Build ();
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
 
-            services.AddMvc (options => { options.Filters.Add (new AuthorizeFilter (policy)); });
+            services.AddMvc(options => { options.Filters.Add(new AuthorizeFilter(policy)); });
 
-            services.AddAuthentication (options => {
+            services.AddAuthentication(options =>
+            {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer (options => {
-                options.Authority = securitySettings.Authority;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = settings.Authority + settings.TenantId;
                 options.TokenValidationParameters.ValidateLifetime = true;
-                options.Audience = securitySettings.ClientId;
+                options.Audience = settings.ClientId;
                 options.TokenValidationParameters.ClockSkew = TimeSpan.Zero;
             });
 
-            services.AddAuthorization ();
+            services.AddAuthorization();
         }
 
         /// <summary>
@@ -93,49 +107,64 @@ namespace ServiceWebsite {
         /// </summary>
         /// <param name="app"></param>
         /// <param name="env"></param>
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (!env.IsProduction ()) {
-                app.UseSwagger ();
-                app.UseSwaggerUI (c => { c.SwaggerEndpoint ("/swagger/v1/swagger.json", "Video Hearings Website backend"); });
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (!env.IsProduction())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    const string url = "/swagger/v1/swagger.json";
+                    c.SwaggerEndpoint(url, "Video Hearings Website backend");
+                });
             }
 
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
-            } else {
-                app.UseExceptionHandler ("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts ();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // this will route any unhandled exceptions to the angular error page
+                app.UseExceptionHandler(Urls.Error);
+                app.UseHsts();
             }
 
-            if (!env.IsDevelopment ()) {
-                app.UseSpaStaticFiles ();
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
             }
 
-            app.UseRouting ();
-            app.UseAuthorization ();
-            app.UseAuthentication ();
-            app.UseHttpsRedirection ();
-            app.UseMiddleware<ExceptionMiddleware> ();
-            app.UseCors ("CorsPolicy");
+            app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-            app.UseEndpoints (endpoints => { endpoints.MapDefaultControllerRoute (); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            
 
             // HTTP Response Headers
-            app.UseXContentTypeOptions ();
-            app.UseReferrerPolicy (opts => opts.NoReferrer ());
-            app.UseXXssProtection (options => options.EnabledWithBlockMode ());
-            app.UseNoCacheHttpHeaders ();
-            app.UseHsts (options => options.MaxAge (365).IncludeSubdomains ());
-            app.UseXfo (options => options.SameOrigin ());
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseNoCacheHttpHeaders();
+            app.UseHsts(options => options.MaxAge(365).IncludeSubdomains());
+            app.UseXfo(options => options.SameOrigin());
 
-            app.UseSpa (spa => {
+            app.UseSpa(spa =>
+            {
                 // Make the source folder relative to content to allow integration test project to run as well
-                var sourcePath = Path.Combine (env.ContentRootPath, "ClientApp");
+                var sourcePath = Path.Combine(env.ContentRootPath, "ClientApp");
                 spa.Options.SourcePath = sourcePath;
 
-                if (env.IsDevelopment ()) {
+                if (env.IsDevelopment())
+                {
                     // this magically uses the ng serve to host the web app under the same port as the api
-                    spa.UseAngularCliServer (npmScript: "start");
+                    spa.UseAngularCliServer(npmScript: "start");
                 }
             });
         }
