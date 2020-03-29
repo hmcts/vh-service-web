926425928ec92e19a9c311344af17ed1dc449123
Lionel Roche <lionel.roche@hmcts.net>
2020-03-29 17:07:20 +0100

    feature/VIH-5629 : Add version property to healthcheck endpoint - removed the video api health check.

--- ServiceWebsite/ServiceWebsite.UnitTests/Controllers/HealthTests.cs
+++ ServiceWebsite/ServiceWebsite.UnitTests/Controllers/HealthTests.cs
@@ -20,16 +20,14 @@ namespace ServiceWebsite.UnitTests.Controllers
         private HealthCheckController _controller;
         private Mock<IUserApiClient> _userApiClientMock;
         private Mock<IBookingsApiClient> _bookingsApiClientMock;
-        private Mock<IVideoApiClient> _videoApiClientMock;
 
         [SetUp]
         public void Setup()
         {
             _userApiClientMock = new Mock<IUserApiClient>();
             _bookingsApiClientMock = new Mock<IBookingsApiClient>();
-            _videoApiClientMock = new Mock<IVideoApiClient>();
 
-            _controller = new HealthCheckController(_userApiClientMock.Object, _bookingsApiClientMock.Object, _videoApiClientMock.Object);
+            _controller = new HealthCheckController(_userApiClientMock.Object, _bookingsApiClientMock.Object);
 
             var judges = Builder<UserResponse>.CreateListOfSize(3).Build().ToList();
             _userApiClientMock.Setup(x => x.GetJudgesAsync())
@@ -88,42 +86,6 @@ namespace ServiceWebsite.UnitTests.Controllers
         }
 
         [Test]
-        public async Task Should_return_internal_server_error_result_when_video_api_not_reachable()
-        {
-            var exception = new VideoApiServiceException<ProblemDetails>("Bad token", (int)HttpStatusCode.InternalServerError,
-                "Please provide a valid conference Id", null, default, null);
-
-            _videoApiClientMock
-                .Setup(x => x.GetConferencesTodayForAdminAsync())
-                .ThrowsAsync(exception);
-
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult)result;
-            typedResult.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
-            var response = (Models.HealthCheckResponse)typedResult.Value;
-            response.VideoApiHealth.Successful.Should().BeFalse();
-            response.VideoApiHealth.ErrorMessage.Should().NotBeNullOrWhiteSpace();
-        }
-
-        [Test]
-        public async Task Should_return_forbidden_error_result_when_video_api_not_reachable()
-        {
-            var exception = new VideoApiServiceException<ProblemDetails>("Unauthorised Token", (int)HttpStatusCode.Unauthorized,
-                "Invalid Client ID", null, default, null);
-
-            _videoApiClientMock
-                .Setup(x => x.GetConferencesTodayForAdminAsync())
-                .ThrowsAsync(exception);
-
-            var result = await _controller.Health();
-            var typedResult = (ObjectResult)result;
-            typedResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
-            var response = (Models.HealthCheckResponse)typedResult.Value;
-            response.VideoApiHealth.Successful.Should().BeTrue();
-            response.VideoApiHealth.ErrorMessage.Should().BeNullOrWhiteSpace();
-        }
-
-        [Test]
         public async Task Should_return_ok_when_all_services_are_running()
         {
             var result = await _controller.Health();
@@ -133,7 +95,6 @@ namespace ServiceWebsite.UnitTests.Controllers
             var response = (Models.HealthCheckResponse)typedResult.Value;
             response.BookingsApiHealth.Successful.Should().BeTrue();
             response.UserApiHealth.Successful.Should().BeTrue();
-            response.VideoApiHealth.Successful.Should().BeTrue();
         }
 
         [Test]
