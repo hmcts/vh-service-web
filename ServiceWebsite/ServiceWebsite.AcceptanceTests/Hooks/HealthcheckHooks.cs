eb7e46bafc825fde1b691cae25d5c880dc2cf9d3
npoxon <npoxon@users.noreply.github.com>
2020-10-19 18:19:45 +0200

    VIH-5255 Retry on Forbidden (#601)
    * testfix/VIH-5255 : Retry on Forbidden
    
    * testfix/VIH-5255 : dotnet restored packages
    
    Co-authored-by: Nicholas Poxon <NicholasPoxon1@hmcts.net>

--- ServiceWebsite/ServiceWebsite.AcceptanceTests/Hooks/HealthcheckHooks.cs
+++ ServiceWebsite/ServiceWebsite.AcceptanceTests/Hooks/HealthcheckHooks.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using FluentAssertions;
+using Polly;
+using RestSharp;
 using ServiceWebsite.AcceptanceTests.Helpers;
 using TechTalk.SpecFlow;
 
@@ -8,12 +11,22 @@ namespace ServiceWebsite.AcceptanceTests.Hooks
     [Binding]
     public class HealthcheckHooks
     {
+        private const int RETRIES = 4;
+
         [BeforeScenario(Order = (int)HooksSequence.HealthcheckHooks)]
         public void CheckApiHealth(TestContext context)
         {
-            var response = context.Api.HealthCheck();
-            response.StatusCode.Should().Be(HttpStatusCode.OK,
-                $"Healthcheck failed with '{response.StatusCode}' and error message '{response.ErrorMessage}'");
+            var retryOnForbiddenFirewallExceptions = Policy
+                .HandleResult<IRestResponse>(r => r.StatusCode == HttpStatusCode.Forbidden)
+                .WaitAndRetry(RETRIES, retryAttempt =>
+                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timeSpan) =>
+                    {
+                        NUnit.Framework.TestContext.WriteLine($"Encountered error '{exception.Result.StatusCode}' after {timeSpan.Seconds} seconds. Retrying...");
+                    });
+
+            var response = retryOnForbiddenFirewallExceptions.Execute(() => context.Api.HealthCheck());
+            response.StatusCode.Should().Be(HttpStatusCode.OK, $"Healthcheck failed with '{response.StatusCode}' and error message '{response.ErrorMessage}'");
         }
     }
 }
