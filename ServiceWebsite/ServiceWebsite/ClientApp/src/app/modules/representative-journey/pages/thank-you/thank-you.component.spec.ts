76c249de5b720ca6da6670c667273bbe71ff39c9
Valentina <Valentina.bennett@hmcts.net>
2020-11-18 18:11:25 +0000

    feature/VIH-6620 : lint issues

--- ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/thank-you/thank-you.component.spec.ts
+++ ServiceWebsite/ServiceWebsite/ClientApp/src/app/modules/representative-journey/pages/thank-you/thank-you.component.spec.ts
@@ -4,23 +4,16 @@ import { ParticipantSuitabilityModel, Hearing } from '../../../base-journey/part
 
 describe('ThankYouComponent', () => {
     it('should on init get hearing details', () => {
-        let representativeJourneyServiceSpy = jasmine.createSpyObj<RepresentativeJourneyService>('RepresentativeJourneyService', ['get']);
-        let currentModel = new ParticipantSuitabilityModel();
+        const representativeJourneyServiceSpy = jasmine.createSpyObj<RepresentativeJourneyService>('RepresentativeJourneyService', ['get']);
+        const currentModel = new ParticipantSuitabilityModel();
         const date = new Date('12 12 2020');
 
-        currentModel.hearing = new Hearing(
-            '12345',
-            date,
-            null,
-            null,
-            false
-        );
+        currentModel.hearing = new Hearing('12345', date, null, null, false);
         representativeJourneyServiceSpy.get.and.returnValue(currentModel);
         const component = new ThankYouComponent(representativeJourneyServiceSpy);
 
         component.ngOnInit();
 
         expect(representativeJourneyServiceSpy.get).toHaveBeenCalled();
-      
-  });
+    });
 });
