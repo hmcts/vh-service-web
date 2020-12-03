391f96f15b274b4ad74675d250c33d308ee4fcfd
Valentina <Valentina.bennett@hmcts.net>
2020-12-03 11:19:41 +0000

    feature/VIH-6620 : check for null in AC

--- ServiceWebsite/ServiceWebsite.AcceptanceTests/Hooks/DataHooks.cs
+++ ServiceWebsite/ServiceWebsite.AcceptanceTests/Hooks/DataHooks.cs
@@ -101,9 +101,9 @@ namespace ServiceWebsite.AcceptanceTests.Hooks
             var response = api.GetSuitabilityAnswers(_username);
             var answers = RequestHelper.Deserialise<List<PersonSuitabilityAnswerResponse>>(response.Content);
 
-            if ( answers != null && answers.First().Answers.Count > 0)
+            if ( answers != null && answers.Count > 0)
             {
-                throw new DataException($"user with username '{_username}' has {answers.First().Answers.Count} previous answer(s) saved");
+                throw new DataException($"user with username '{_username}' has {answers.Count} previous answer(s) saved");
             }
         }
     }
